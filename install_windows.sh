744aa3f8ad1ac9f2c03b91b236d331c8b8207f5f
howientc <root@howie>
2016-01-12 16:04:52 -0800

    modified pkg-config prefix

--- install_windows.sh
+++ install_windows.sh
@@ -8,10 +8,11 @@ mkdir windows && pushd windows
 curl http://pkg.mxe.cc/repos/tar/mxe-i686-w64-mingw32.static/mxe-i686-w64-mingw32.static-libusb1_1.0.19.tar.xz | tar xvJ
 
 #modify the pkg-config file for libusb so it points here
+pushd usr/i686-w64-mingw32.static
 find -name '*.pc' | while read pc; do sed -e "s@^prefix=.*@prefix=$PWD@" -i "$pc"; done
 
-export PKG_CONFIG_PATH=$PWD/usr/i686-w64-mingw32.static/lib/pkgconfig
-
+export PKG_CONFIG_PATH=$PWD/lib/pkgconfig
+popd
 popd
 make fel
 
